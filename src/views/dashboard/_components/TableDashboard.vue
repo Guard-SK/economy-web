069318793073747cd613a32670b4d3d02bc9b306
Benik1234 <65073847+Benik1234@users.noreply.github.com>
2023-01-26 20:51:53 +0100

    repa

--- src/views/dashboard/_components/TableDashboard.vue
+++ src/views/dashboard/_components/TableDashboard.vue
@@ -170,37 +170,11 @@ methods:{
             const downloadRef = ref(storage,fileref)
             getDownloadURL(downloadRef)
                 .then((url) => {
-   
-
-
                 const link = document.createElement("a");
                 link.href = url;
                 link.download = 'readme.txt' ;
                 link.click()
-
-                    
-
-                        }).catch((error) => {
-    // A full list of error codes is available at
-    // https://firebase.google.com/docs/storage/web/handle-errors
-    switch (error.code) {
-      case 'storage/object-not-found':
-        // File doesn't exist
-        break;
-      case 'storage/unauthorized':
-        // User doesn't have permission to access the object
-        break;
-      case 'storage/canceled':
-        // User canceled the upload
-        break;
-
-      // ...
-
-      case 'storage/unknown':
-        // Unknown error occurred, inspect the server response
-        break;
-    }
-                    })}
+            })}
     },
     async transactions(event) {
         try {
