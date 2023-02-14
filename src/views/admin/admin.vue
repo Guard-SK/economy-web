933803cb582e59391169da988e63af34cadeb5b4
Benik1234 <65073847+Benik1234@users.noreply.github.com>
2023-02-14 19:00:11 +0100

    update

--- src/views/admin/admin.vue
+++ src/views/admin/admin.vue
@@ -1,5 +1,14 @@
 <template>
     <p>Priprava Admin</p>
+    <iframe style="width:100%; height:1080px; "  src="https://docs.google.com/document/d/e/2PACX-1vSgDyciwaeu7qx7HITuyE2pFro7XKKmyBvQDKk0PtNKx46AEx7TdvFV6eWXcJDHmA/pub?embedded=true"></iframe>
 </template>
 <script>
+export default {
+created() {
+    console.log('lol')
+    
+},
+}
+
+
 </script>
