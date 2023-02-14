b1d5e8fba9efb5e3f07ea05abb4ce84186552909
Benik1234 <65073847+Benik1234@users.noreply.github.com>
2023-02-14 23:12:13 +0100

    c

--- src/views/admin/admin.vue
+++ src/views/admin/admin.vue
@@ -3,7 +3,7 @@
     
 
     <Dropdown v-model="selectedUser" :options="users" optionLabel="name" :filter="true" placeholder="Select a user" :showClear="true" @change="userset"></Dropdown>
-    <iframe src="https://docs.google.com/document/d/e/2PACX-1vSgDyciwaeu7qx7HITuyE2pFro7XKKmyBvQDKk0PtNKx46AEx7TdvFV6eWXcJDHmA/pub?embedded=true"  style="width: calc(100% + 80px); margin-left: -80px"></iframe>
+    <iframe height=900px src="https://docs.google.com/document/d/e/2PACX-1vSgDyciwaeu7qx7HITuyE2pFro7XKKmyBvQDKk0PtNKx46AEx7TdvFV6eWXcJDHmA/pub?embedded=true"  style=" width: calc(100% + 80px); margin-left: -80px" ></iframe>
 
 
 </template>
