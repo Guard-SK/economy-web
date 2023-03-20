af1dcc7ea37f9ce90b6cc866adc8ac8329cbadb8
Benik1234 <65073847+Benik1234@users.noreply.github.com>
2023-03-20 19:46:41 +0100

    Update admin.vue

--- src/views/admin/admin.vue
+++ src/views/admin/admin.vue
@@ -6,6 +6,8 @@
             <h1 class="font-semibold spacing  text-xl ">Posuvatelne menu</h1>
             <TabMenu class="mt-5 " style="align: center" :model="items" :activeIndex="activeIndex" >
             </TabMenu>
+            <button class="btn btn-primary px-auto" v-on:click="recalculate">Prepočítať</button>
+            
             
 
           
@@ -59,6 +61,55 @@ export default {
       }
   },
   methods:{
+    async recalculate() {
+            const db = getFirestore()
+            const events = await getDocs(collection(db,'events'))
+            const users = await getDocs(collection(db,'users'))
+            users.forEach(async user => {
+                let uid = user.id
+                let username = user.data().name + ' ' + user.data().surname
+                let usernameset = user.data().name + ' ' + user.data().surname + 'set'
+                let costsofficial =[0]
+                let costsunofficial = [0]
+                events.forEach(event => {
+
+                    if (event.data()[username] == "✅"){
+                        if (event.data()[usernameset] != false) {
+                            if (event.data().typeoffond == 'official') {
+                            costsofficial.push(parseFloat(event.data()[usernameset]))
+                            }else {
+                            costsunofficial.push(parseFloat(event.data()[usernameset]))
+                            }
+                        }else{
+                            var eventdata = event.data()
+                            const arr = Object.values(eventdata)
+                            var count = arr.filter(function(value) {
+                                return value === "✅";
+                            }).length;
+                            count -= event.data().eventnumberset
+                            var eventcost = event.data().costofevent - event.data().eventcostset
+                            var cpp = eventcost / count
+                            if (event.data().typeoffond == 'official'){
+                                costsofficial.push(cpp)
+    
+                            }else {
+                                costsunofficial.push(cpp)
+                            } 
+                        }
+                    }
+                })
+                const sum = costsofficial.reduce((accumulator, currentValue) => {
+                    return accumulator + currentValue;
+                }, 0);
+                const sum2 = costsunofficial.reduce((accumulator, currentValue) => {
+                    return accumulator + currentValue;
+                }, 0);
+                const baloff = sum + user.data().positivebalanceofficial
+                const balunoff = sum2 + user.data().positivebalanceunofficial
+
+                await setDoc(doc(db,'users',uid),{balanceofficial: parseFloat(baloff.toFixed(2)),balanceunofficial: parseFloat(balunoff.toFixed(2)),},{merge:true})
+            })
+        },
       async userset(){
           console.log(this.selectedUser.uid)
       },
